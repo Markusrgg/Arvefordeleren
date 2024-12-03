484f5906dc0c990202547f996f8a8bce77338bf0
Markusrgg <rggmarkus@gmail.com>
2024-12-03 12:01:38 +0100

    Notes

--- Arvefordeleren/Arvefordeleren_ClassLibrary/Models/ICloneableOfT.cs
+++ Arvefordeleren/Arvefordeleren_ClassLibrary/Models/ICloneableOfT.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Arvefordeleren_ClassLibrary.Models
 {
-    // Clone interface, forced upon non-abstract methods to allow Repository to deep-copy our _list and not just return reference types to the same objects of the original list.
+    // Clone interface, forced upon non-abstract methods to allow Repository to deep-copy our _list
+    // and not just return reference types to the same objects of the original list.
     public interface ICloneable<T>
     {
         T Clone();
